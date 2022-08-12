bfd68f60ea8a2464aafe337a9ee3469803c14fef
Lab Computo <Computosala42022@ugb.edu.sv>
2022-08-12 10:44:47 -0600

    Agregar archivos de proyecto.

---
+++ 120822/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _120822
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
