050791513b3199b68a6fb1e2c7cbbb88d8abf89f
Lab Computo <Computosala42022@ugb.edu.sv>
2022-08-12 11:07:27 -0600

    1104

--- 120822/Program.cs
+++ 120822/Program.cs
@@ -6,7 +6,22 @@ namespace _120822
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("ingrese su nota promedio:");
+            double nota = double.Parse(Console.ReadLine());
+
+            if (nota >= 0 && nota <= 10) ;
+            
+            if (nota >= 6)
+
+                Console.WriteLine("aprobado");
+            else if (nota >= 5)
+                Console.WriteLine("reposicion");
+            else
+                Console.WriteLine("reprobado");
+
+            else
+                Console.WriteLine("la nota no es correcta");
         }
     }
+
 }
